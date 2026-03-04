4ca70fc559871b1ad513a5bdc2dc2cdd1bd4a6f6
Ritesh Ghavat <riteshghavat898@example.com>
2026-03-05 00:31:16 +0530

    update database

--- api/database.py
+++ api/database.py
@@ -1,9 +1,14 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, declarative_base
+import os
+from fastapi import FastAPI, Depends
+from sqlalchemy import create_engine, text
+from sqlalchemy.orm import sessionmaker, declarative_base, Session
 
-DATABASE_URL = "postgresql://postgres:1234@localhost:5432/hospital_analytics"
+DATABASE_URL = os.getenv("DATABASE_URL")
 
-engine = create_engine(DATABASE_URL)
+engine = create_engine(
+    DATABASE_URL,
+    connect_args={"sslmode": "require"}  # Required for Render
+)
 
 SessionLocal = sessionmaker(
     autocommit=False,
@@ -13,7 +18,7 @@ SessionLocal = sessionmaker(
 
 Base = declarative_base()
 
-
+app = FastAPI()
 
 def get_db():
     db = SessionLocal()
@@ -21,3 +26,8 @@ def get_db():
         yield db
     finally:
         db.close()
+
+@app.get("/test-db")
+def test_db(db: Session = Depends(get_db)):
+    result = db.execute(text("SELECT 1"))
+    return {"message": "Database connected", "result": result.scalar()}
