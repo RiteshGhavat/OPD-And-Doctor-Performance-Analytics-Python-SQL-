748e403b6a9f86bef0cf3d085b98d88ae574482a
Ritesh Ghavat <riteshghavat898@example.com>
2026-03-05 20:13:17 +0530

    vercel changes

--- api/main.py
+++ api/main.py
@@ -1,39 +1,46 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from database import engine, Base
-from routes import auth_routes, register_routes
-from models import models  
-from routes.admin import dashboard
-from routes.admin import doctor
-from routes.admin import branch_router
-from routes.admin import users
-from routes.admin.analytics import overview
-from routes.admin.analytics import doctor_analytics
-from routes.admin.analytics import revenue
-from routes.admin.analytics import peak_hours
-from routes.users import userdashboard
+
+from api.database import engine, Base
+from api.routes import auth_routes, register_routes
+from api.models import models
+
+from api.routes.admin import dashboard, doctor, branch_router, users
+from api.routes.admin.analytics import overview, doctor_analytics, revenue, peak_hours
+from api.routes.users import userdashboard
 
 app = FastAPI(title="Hospital Analytics API")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
+# ❌ DO NOT RUN create_all() on Vercel
+# Base.metadata.create_all(bind=engine)
 
-Base.metadata.create_all(bind=engine)
+@app.get("/")
+def root():
+    return {"status": "Hospital Analytics API running"}
+
+@app.get("/health")
+def health():
+    return {"health": "ok"}
 
 app.include_router(register_routes.router)
 app.include_router(auth_routes.router)
+
 app.include_router(dashboard.router)
 app.include_router(doctor.router)
 app.include_router(branch_router.router)
 app.include_router(users.router)
+
 app.include_router(overview.router)
 app.include_router(doctor_analytics.router)
 app.include_router(revenue.router)
 app.include_router(peak_hours.router)
+
 app.include_router(userdashboard.router)
